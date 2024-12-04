66185332acf3341b6aa39dba5e2fe105077230d4
AcademyFenceCompany <info@academyfence.com>
2024-12-04 09:55:10 -0500

    Cleaned up working directory

--- products_transform.py
+++ products_transform.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
-original_products_df = pd.read_csv("products_ORIGINAL.csv")
+# PRODUCT SCHEMA AND MAPPING TO NEW DATABASE
 
-products_df = pd.DataFrame(columns=['product_id', 'product_name', 'item_no', 'price_per_unit', 'shippable', 'family_category_id', 'description'])
-product_details_df = pd.DataFrame(columns=['product_details_id', 'product_id', 'size1', 'size2', 'size3', 'style', 'speciality', 'material', 'spacing', 'color', 'coating'])
-shipping_details_df = pd.DataFrame(columns=['shipping_details_id', 'product_id', 'weight', 'shipping_length', 'shipping_width', 'shipping_height', 'shipping_class', 'description'])
-product_media_df = pd.DataFrame(columns=['product_media_id', 'product_id', 'general_image', 'small_image', 'large_image'])
+# products_df = pd.DataFrame(columns=['product_id', 'product_name', 'item_no', 'price_per_unit', 'shippable', 'family_category_id', 'description'])
+# product_details_df = pd.DataFrame(columns=['product_details_id', 'product_id', 'size1', 'size2', 'size3', 'style', 'speciality', 'material', 'spacing', 'color', 'coating'])
+# shipping_details_df = pd.DataFrame(columns=['shipping_details_id', 'product_id', 'weight', 'shipping_length', 'shipping_width', 'shipping_height', 'shipping_class', 'description'])
+# product_media_df = pd.DataFrame(columns=['product_media_id', 'product_id', 'general_image', 'small_image', 'large_image'])
+
 # loop that runs jobs parallel 
 # - one for inserting data into product table
 #           product_id?
@@ -27,9 +28,3 @@ product_media_df = pd.DataFrame(columns=['product_media_id', 'product_id', 'gene
 #           ship_height
 #           class
 # Starting with fence slats - cat id 42 (but that doesnt matter anyways bc all fence slat products cat_id_fk are NULL in the database :))
-def check_dataframe():
-    # df.to_csv("products_TRANSFORMED.csv")
-    # print(df.head(1).to_markdown())
-    print(original_products_df)
-
-check_dataframe()
